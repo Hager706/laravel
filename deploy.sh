8abaa9b23a8035a0f45a8b14c3a106861f44fc59
Hager706 <hagertarek706@gmail.com>
2025-11-23 00:41:23 +0200

    add backend workflow

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -e
 
+# Update system and install PHP 8.2 (if not already)
+sudo apt update
+sudo apt install -y software-properties-common
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt update
+sudo apt install -y php8.2 php8.2-cli php8.2-mbstring php8.2-xml php8.2-zip php8.2-curl unzip
+
+# Ensure PHP 8.2 is default
+sudo update-alternatives --set php /usr/bin/php8.2
+
+# Install Composer if not present
+if ! command -v composer &> /dev/null; then
+  curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+fi
+
 git pull origin 12.x
 
 # Install/update dependencies
