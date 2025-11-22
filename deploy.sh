ea531e3d1c57be2451fafdbaa1e45bdae35d7751
Hager706 <hagertarek706@gmail.com>
2025-11-23 00:18:32 +0200

    add backend workflow

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,8 @@
 set -e
 
 git pull origin 12.x
-sudo apt update
-sudo apt install -y php php-cli php-mbstring php-xml php-zip unzip
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+# Install/update dependencies
 composer install --no-dev --optimize-autoloader
 
 # Run migrations (assumes .env is configured with RDS details)
