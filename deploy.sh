361993fb4726fcb1014986034649fd291e7d7bbf
Hager706 <hagertarek706@gmail.com>
2025-11-23 05:33:56 +0200

    add backend workflow

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,10 @@ if ! command -v composer &> /dev/null; then
   curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 fi
 
+echo "Pull latest code..."
+git stash push -m "auto-stash" || true
 git pull origin 12.x
+git stash pop || true
 
 # Install/update dependencies
 /usr/local/bin/composer install --no-dev --optimize-autoloader
